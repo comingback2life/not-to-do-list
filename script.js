5efad12e6d39dee9205667d1abf264dee6cf6d6e
Samip <callmesamip@gmail.com>
2022-02-15 10:48:06 +1100

    Fixed all the bugs and updated the not to do list

--- script.js
+++ script.js
@@ -6,15 +6,16 @@ const handleOnSubmit =e =>{ //handle on submit is being triggered everytime the 
   const frmData = new FormData(e);
   const task = frmData.get("task");
   const hours = +frmData.get("hr") //total hours being added.
-  if(hours<0){
+  if(hours<1){
     return alert("How can time be less than 0 ??");
   };
+  const ttlBadHours = totalBadHours();
     const doNotDo = {
       task,
       hours
     }
-
-    if((savedHours()+hours)>totalWeeklyHours){
+    
+    if((savedHours()+hours+ttlBadHours)>totalWeeklyHours){
       alert("The total number of hours in a week is 168");
     }else{
       taskList.push(doNotDo); //pushing the tasklist to the array to store it.
@@ -76,13 +77,13 @@ const displayNotToDoTask=()=>{
       if(item.hours===1){
         str+=`<td> ${item.hours} hr</td>
         <td class="text-end">
-        <button class="btn btn-sm btn-warning" onclick="doNotDoTask(${index})">
+        <button class="btn btn-sm btn-warning" onclick="markAsToDo(${index})">
             <i
               class="fa-solid fa-arrow-left"
               title="Mark as bad list"
             ></i>
           </button>
-          <button class="btn btn-danger btn-sm" onclick="deleteItem(${index})">
+          <button class="btn btn-danger btn-sm" onclick="deleteBadItem(${index})">
             <i class="fa-solid fa-trash" title="Delete"></i>
           </button>
           
@@ -91,15 +92,16 @@ const displayNotToDoTask=()=>{
       }else{
         str+=`<td> ${item.hours} hrs</td>
         <td class="text-end">
-          <button class="btn btn-danger btn-sm" onclick="deleteItem(${index})">
-            <i class="fa-solid fa-trash" title="Delete"></i>
-          </button>
-          <button class="btn btn-sm btn-warning" onclick="doNotDoTask(${index})">
+        
+          <button class="btn btn-sm btn-warning" onclick="markAsToDo(${index})">
             <i
               class="fa-solid fa-arrow-left"
               title="Mark as to-do list"
             ></i>
           </button>
+          <button class="btn btn-danger btn-sm" onclick="deleteBadItem(${index})">
+          <i class="fa-solid fa-trash" title="Delete"></i>
+        </button>
         </td>
       </tr>`
       };
@@ -116,9 +118,21 @@ const deleteItem=(index)=>{
   displayTask();
   savedHours();
 }
+const deleteBadItem=(index)=>{
+  if(!confirm("Do you really want to delete this ?")){
+   return;
+  }
+
+  badList.splice(index,1); // remove one item from the index position 
+  displayNotToDoTask();
+  totalBadHours();
+}
+
 
 const savedHours=()=>{
   const total = taskList.reduce((subtotal,i)=>subtotal+i.hours,0)
+
+
   if(total===0){
     document.getElementById('totalHours').innerText="";
   }else if(total===1){
@@ -129,10 +143,32 @@ const savedHours=()=>{
   }
   return total;
 }
+const totalBadHours = ()=>{
+  const ttlBadHours = badList.reduce((subttl,item)=>subttl+item.hours,0);
+  if(ttlBadHours===0){
+    document.getElementById('totalBadHr').innerText="";
+  }else if(ttlBadHours===1){
+    document.getElementById('totalBadHr').innerText=(`Total time saved this week ${ttlBadHours} hr`);
+  }
+  else{
+    document.getElementById('totalBadHr').innerText=(`Total time saved this week ${ttlBadHours} hrs`);
+  }
+  return ttlBadHours;
 
+}
 const doNotDoTask = i =>{
   const itm= taskList.splice(i,1);
+  displayTask();
   badList.push(itm[0]);
   displayNotToDoTask();
+  totalBadHours();
+  savedHours();
+}
+const markAsToDo = i =>{
+  const itm= badList.splice(i,1);
+  displayNotToDoTask();
+  taskList.push(itm[0]);
   displayTask();
+  totalBadHours();
+  savedHours();
 }
